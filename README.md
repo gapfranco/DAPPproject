3e8c3555fde9cfd05caff8e5bcc74198d1308a6a
Goncalo Franco <gapfranco@gmail.com>
2019-01-09 22:05:49 -0200

    Final version

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ Truffle project to deploy ERC-721 tokens and a simple DAPP
 ### Setup
 
 1. Install Node and NPM or yarn
-2. Clone the project - `git clone https://github.com/gapfranco/dapp-project`
+2. Clone the project - `git clone https://github.com/gapfranco/DAPPproject`
 3. Install dependencies - `npm install` or `yarn`
-4. Create a .env file on project root with the Metamask mneumonic and the Infura key as
-   environent variables (see include example in env.sample)
+4. Install truffle - npm install truffle@4.1.15 -g
 
 ### Deploying the token
 
@@ -23,8 +22,7 @@ test
 
 deploy --reset (to deploy to the private network with Metamask)
 
-deploy --reset --netword rinkeby (to deploy to Rinkeby)
-deploy --reset --netword ropsten (to deploy to Ropsten)
+deploy --reset --netword rinkeby
 
 ```
 
@@ -44,4 +42,4 @@ Access with web browser at port 8080.
 | ------------------------------------ | ------------------------------------------ |
 | ERC-721 Token Name                   | Star Notary                                |
 | ERC-721 Token Symbol                 | STN                                        |
-| Token Address on the Ropsten Network | 0x65f3c6f5dfeeb7a51476a0d1c5575c91c415cdda |
+| Token Address on the Rinkeby Network | 0xab19145b84512a45d92ed0cf2f4342d4e694f4a1 |
--- truffle.js
+++ truffle.js
@@ -1,16 +1,14 @@
 // Allows us to use ES6 in our migrations and tests.
-require("babel-register")
+require("babel-register");
 
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
 
-// 8D5366123CB560BB606379F90A0BFD4769EECC0557F1B362DCAE9012B548B1E5
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
-const mnemonic = "zone omit spot game rude toddler swarm museum future letter ranch donkey"
+const infuraKey = "601a05417e72468398dca54d9a730d76";
 
-const infuraKey = "601a05417e72468398dca54d9a730d76"
-
-const HDWalletProvider = require("truffle-hdwallet-provider")
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
@@ -19,25 +17,11 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    ropsten: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          "https://ropsten.infura.io/v3/" + infuraKey
-        ),
-      network_id: 3,
-      gas: 4000000,
-      gasPrice: 1000000000
-    },
     rinkeby: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          "https://rinkeby.infura.io/v3/" + infuraKey
-        ),
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey),
       network_id: 4,
       gas: 4000000,
       gasPrice: 1000000000
     }
   }
-}
+};
